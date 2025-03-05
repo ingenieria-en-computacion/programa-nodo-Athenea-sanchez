6340b6140868c94a488ebb3d2208e5859e804d51
Athenea Sánchez Torres <sancheztorresathenea@gmail.com>
2025-03-04 19:58:44 -0600

    Nodos

---
+++ nodos.h
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+#ifndef _NODO_
+#define _NODO_
+#include <stdbool.h>
+
+  //TAD
+
+ typedef int DATO; 
+ typedef struct _Nodo Nodo; 
+ 
+ typedef struct _Nodo{
+  DATO dato; 
+  Nodo* sig; 
+ } Nodo; 
+
+ // Funciones 
+Nodo* CrearNodo (); 
+bool BorrarNodo (Nodo*); 
+Nodo* AsignarDato (Nodo*, DATO); 
+DATO ObterDato (Nodo*); 
+Nodo* AsignarReferencia (Nodo*, Nodo*); 
+Nodo* ObtenerRef (Nodo*);  
+
+
+#endif
